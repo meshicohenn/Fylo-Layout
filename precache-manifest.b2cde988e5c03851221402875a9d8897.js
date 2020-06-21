ff54935c93a44b2125e8cc13c47904607e874e42
meshicohenn <meshicohenn@gmail.com>
2020-06-21 17:38:45 +0300

    Updates

---
+++ precache-manifest.b2cde988e5c03851221402875a9d8897.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff50da45af524ecde57cb9b589adc3c",
+    "url": "/Fylo-Layout/index.html"
+  },
+  {
+    "revision": "100fa82df9fe1ff98177",
+    "url": "/Fylo-Layout/static/css/main.48bda4b5.chunk.css"
+  },
+  {
+    "revision": "ac4eb3cfbbbbf0a21633",
+    "url": "/Fylo-Layout/static/js/2.24384f86.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Fylo-Layout/static/js/2.24384f86.chunk.js.LICENSE"
+  },
+  {
+    "revision": "100fa82df9fe1ff98177",
+    "url": "/Fylo-Layout/static/js/main.64e7148d.chunk.js"
+  },
+  {
+    "revision": "d5ebf117153a91343d19",
+    "url": "/Fylo-Layout/static/js/runtime-main.481cc464.js"
+  },
+  {
+    "revision": "b20ff458bc0dc48c039c68a3dc96173c",
+    "url": "/Fylo-Layout/static/media/Caveat-Regular.b20ff458.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Fylo-Layout/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/Fylo-Layout/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/Fylo-Layout/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "1727d7d1fb2882b66168996105d82a38",
+    "url": "/Fylo-Layout/static/media/Raleway-Bold.1727d7d1.ttf"
+  },
+  {
+    "revision": "8c1cd7ead51757b47fcc43bf4413279b",
+    "url": "/Fylo-Layout/static/media/Raleway-ExtraLight.8c1cd7ea.ttf"
+  },
+  {
+    "revision": "87d9b5671151b4ffae1a8b3d4a02ed67",
+    "url": "/Fylo-Layout/static/media/Raleway-Light.87d9b567.ttf"
+  },
+  {
+    "revision": "83dc6a3bc3f2c02512106db5c76fd52a",
+    "url": "/Fylo-Layout/static/media/Raleway-Medium.83dc6a3b.ttf"
+  },
+  {
+    "revision": "ab90c33bd623a29195d2cb3a3cbae6f5",
+    "url": "/Fylo-Layout/static/media/Raleway-Regular.ab90c33b.ttf"
+  },
+  {
+    "revision": "3e21369420e23cbb3bb81de7b644298d",
+    "url": "/Fylo-Layout/static/media/Raleway-Thin.3e213694.ttf"
+  },
+  {
+    "revision": "5f4b8b3e0d56ac7578b1468584142faf",
+    "url": "/Fylo-Layout/static/media/icon-access-anywhere.5f4b8b3e.svg"
+  },
+  {
+    "revision": "aedb3ae079501c43082669d8d427bf6b",
+    "url": "/Fylo-Layout/static/media/icon-any-file.aedb3ae0.svg"
+  },
+  {
+    "revision": "794c5a484aefc6409c3a4e7aceb10eee",
+    "url": "/Fylo-Layout/static/media/icon-arrow.794c5a48.svg"
+  },
+  {
+    "revision": "ca8c71cfbd876e461e850fcdcf9c9b29",
+    "url": "/Fylo-Layout/static/media/icon-collaboration.ca8c71cf.svg"
+  },
+  {
+    "revision": "c90d96c2492a2d6f0863256f2bf9015d",
+    "url": "/Fylo-Layout/static/media/icon-email.c90d96c2.svg"
+  },
+  {
+    "revision": "0f65814bd9f282828254e3382e223a57",
+    "url": "/Fylo-Layout/static/media/icon-location.0f65814b.svg"
+  },
+  {
+    "revision": "47e54a44b879380ff8adb31f40249296",
+    "url": "/Fylo-Layout/static/media/icon-phone.47e54a44.svg"
+  },
+  {
+    "revision": "f558537850876022cbf1d99e525724f4",
+    "url": "/Fylo-Layout/static/media/icon-security.f5585378.svg"
+  },
+  {
+    "revision": "526544ae64c3d7a9cf06bc566728d2b9",
+    "url": "/Fylo-Layout/static/media/illustration-intro.526544ae.png"
+  },
+  {
+    "revision": "99294b1f2757d664921ec643a982959a",
+    "url": "/Fylo-Layout/static/media/illustration-stay-productive.99294b1f.png"
+  },
+  {
+    "revision": "c71bf7bf6985f3ac03fa893fb45bed38",
+    "url": "/Fylo-Layout/static/media/logo.c71bf7bf.svg"
+  }
+]);
